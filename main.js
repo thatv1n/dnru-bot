f9287510ed76c9e0853deb079fa7425c09987ce3
Ivan <shakhov.ivan.p@gmail.com>
2026-01-19 19:23:05 +0500

    +

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ process.on('unhandledRejection', (reason, promise) => {
 
 setInterval(
 	() => {
-		console.log('✅ Бот активен, Railway не засыпает');
+		console.log('✅ Бот активен, и не спит');
 	},
 	60 * 60 * 1000,
 );
